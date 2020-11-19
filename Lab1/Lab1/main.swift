0092f7c941c3a253c57f45898ff1dafe0067c2df
EugeneMaltsev <no.one.left.disappointed@gmail.com>
2020-11-20 00:20:50 +0200

    Very important function (!)

---
+++ Lab1/Lab1/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  Lab1
+//
+//  Created by Roman Falchuk on 18.11.2020.
+//  Copyright © 2020 Roman Falchuk. All rights reserved.
+//
+
+import Foundation
+import MyMath
+
+func main() -> Int32
+{
+    print("Hello, World!")
+
+    print(MyMath.squareRoot(a:1.0, b:2.0, c:3.0))
+
+    return 0
+}
+
+exit(main())
---
+++ Lab1/MyMath/MyMath.swift
@@ -0,0 +1,16 @@
+//
+//  MyMath.m
+//  MyMath
+//
+//  Created by Roman Falchuk on 18.11.2020.
+//  Copyright © 2020 Roman Falchuk. All rights reserved.
+//
+
+import Foundation
+
+public func squareRoot(a: Double, b: Double, c: Double) -> (Double, Double)
+{
+//    let square: Int = a + b + c
+    return (0,0)
+}
+
